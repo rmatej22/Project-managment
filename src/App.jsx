b03258e3b8ec64aea9de421538b746da677b1317
rmatej22 <rmatej545@gmail.com>
2024-11-11 18:55:57 +0100

    add functionality for adding new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
-import NoProjectSelected from './components/NoProjectSelected.jsx';
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      <NoProjectSelected />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
